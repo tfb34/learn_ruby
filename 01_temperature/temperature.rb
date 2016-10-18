c61257ad2c2887de13bd85070635dd80cc043489
Thalia <tfb34@scarletmail.rutgers.edu>
2016-10-18 13:24:02 -0400

    added 2 functions 'ftoc' and 'ctof' inside temperature.rb and utilized the round method

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+def ftoc(degree)
+	var= (degree-32)/1.8
+	puts var.round(1).to_s
+	return var.round(1)
+end
+def ctof(degree)
+	var =1.8*degree+32
+	puts var.round(1).to_s
+	return var.round(1)
+end
