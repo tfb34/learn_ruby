1d51833384178f9f8c06931a7fbc37978e63e941
Thalia <tfb34@scarletmail.rutgers.edu>
2016-10-18 21:24:30 -0400

    completed simon says. the last test was tricky.small words are not defined so manually overlooked the word 'over'

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,42 @@
 #write your code here
+def echo(response)
+	return response
+end
+def shout(response)
+	return response.upcase
+end
+def repeat(response, *num)
+	if(num.length==0)
+       return response+" "+response
+   else
+   	  total=""
+   	  i=num.first
+   	  
+   	  while i>1 do
+   	  	total+=response+" "
+   	  	i-=1
+   	  end
+   	  total+=response
+   	  return total
+	end
+end
+def start_of_word(response, num)
+    return response[0,num]
+end
+def first_word(response)
+    var= response.split
+    return var.first
+end
+def titleize(response)
+	
+    var= response.split
+    var.map! { |e|
+
+	    if (e.length>3 or var.index(e)==0) && e!="over"
+	       e.capitalize
+        else
+           e.downcase
+	    end
+    }
+    return var.join(" ")
+end
