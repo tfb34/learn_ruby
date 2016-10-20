2a84cb12046552272f04ee763e44f2f3b41e172d
Thalia <tfb34@scarletmail.rutgers.edu>
2016-10-20 16:26:32 -0400

    completed

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,35 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+     @seconds=0
+  	
+  end
+  
+  def time_string
+  	  timeUnits=Array.new(3,"00")
+  	  if @seconds==0
+  	  	return timeUnits.join(":")
+      else
+      	 i=timeUnits.length-1  #begin with the seconds unit
+      	 while i>0
+      	 	timeUnits[i]=padded(@seconds%60)
+      	 	@seconds=@seconds/60
+      	 	i-=1
+      	 end
+  	  end
+  	  timeUnits[0]=padded(@seconds%60)
+  	  return timeUnits.join(":")
+  end
+
+end
+
+def padded(num)
+    if num/10 ==0
+    	return "0"+num.to_s
+    else
+    	return num
+    end
+
 end
