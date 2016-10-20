f7432bc02f58f4496e41835ba9d46f2baae0eeac
Thalia <tfb34@scarletmail.rutgers.edu>
2016-10-19 23:17:06 -0400

    implemented class Book and used methods split,map,gsub

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
 # write your code here
+     attr_accessor :title
+     def title
+
+     	@title=@title.split
+     	@title.map! { |word|  
+     		word.capitalize
+     	}
+     	@title=nopeWords(@title.join(" "))
+     	
+     end
+     def nopeWords(title)
+     	article=[" The ", " A ", " An ", " And "," For "," Nor "," But ",
+     		" Or "," Yet "," So "," At "," By "," For "," In ",
+     		" Of "," On "," To "," With "]
+     	article.each{ |w|
+     		 title=title.gsub(w,w.downcase)
+     	}
+        return title
+     end
+     
+
 end
